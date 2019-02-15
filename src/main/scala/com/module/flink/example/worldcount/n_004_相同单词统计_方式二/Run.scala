286e2db52f30bd47c11c60411ea4609903396dbf
thinktothings <thinktothings@gmail.com>
2019-02-15 10:10:52 +0800

    .keyBy("word")       /**         * 每5秒刷新一次，相当于重新开始计数，         * 好处，不需要一直拿所有的数据统计         * 只需要在指定时间间隔内的增量数据，减少了数据规模         */       .timeWindow(Time.seconds(5))

--- src/main/scala/com/module/flink/example/worldcount/n_004_相同单词统计_方式二/Run.scala
+++ src/main/scala/com/module/flink/example/worldcount/n_004_相同单词统计_方式二/Run.scala
@@ -1,6 +1,7 @@
 package com.module.flink.example.worldcount.n_004_相同单词统计_方式二
 
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.time.Time
 
 
 object Run {
@@ -17,7 +18,14 @@ object Run {
 
     import org.apache.flink.streaming.api.scala._
     val textResult = text.flatMap( w => w.split("\\s") ).map( w => WordWithCount(w,1))
-      .keyBy("word").sum("count" )
+      .keyBy("word")
+      /**
+        * 每5秒刷新一次，相当于重新开始计数，
+        * 好处，不需要一直拿所有的数据统计
+        * 只需要在指定时间间隔内的增量数据，减少了数据规模
+        */
+      .timeWindow(Time.seconds(5))
+      .sum("count" )
 
     textResult.print().setParallelism(1)
 
